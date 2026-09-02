b7b0a3bb389478c89ac840e563d5e10b9e107df3
Angga Prastya Sianipar <anggasianipar2@gmail.com>
2026-09-02 23:07:25 +0700

    Add files via upload

---
+++ db/asrischool.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 19, 2026 at 03:52 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `asrischool`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `countdown`
+--
+-- Error reading structure for table asrischool.countdown: #1932 - Table &#039;asrischool.countdown&#039; doesn&#039;t exist in engine
+-- Error reading data for table asrischool.countdown: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `asrischool`.`countdown`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_pendaftaran`
+--
+-- Error reading structure for table asrischool.data_pendaftaran: #1932 - Table &#039;asrischool.data_pendaftaran&#039; doesn&#039;t exist in engine
+-- Error reading data for table asrischool.data_pendaftaran: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `asrischool`.`data_pendaftaran`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lapor`
+--
+-- Error reading structure for table asrischool.lapor: #1932 - Table &#039;asrischool.lapor&#039; doesn&#039;t exist in engine
+-- Error reading data for table asrischool.lapor: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `asrischool`.`lapor`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran_spp`
+--
+-- Error reading structure for table asrischool.pembayaran_spp: #1932 - Table &#039;asrischool.pembayaran_spp&#039; doesn&#039;t exist in engine
+-- Error reading data for table asrischool.pembayaran_spp: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `asrischool`.`pembayaran_spp`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembelian_seragam`
+--
+-- Error reading structure for table asrischool.pembelian_seragam: #1932 - Table &#039;asrischool.pembelian_seragam&#039; doesn&#039;t exist in engine
+-- Error reading data for table asrischool.pembelian_seragam: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `asrischool`.`pembelian_seragam`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profiladmin`
+--
+-- Error reading structure for table asrischool.profiladmin: #1932 - Table &#039;asrischool.profiladmin&#039; doesn&#039;t exist in engine
+-- Error reading data for table asrischool.profiladmin: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `asrischool`.`profiladmin`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+-- Error reading structure for table asrischool.students: #1932 - Table &#039;asrischool.students&#039; doesn&#039;t exist in engine
+-- Error reading data for table asrischool.students: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `asrischool`.`students`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table asrischool.users: #1932 - Table &#039;asrischool.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table asrischool.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `asrischool`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
